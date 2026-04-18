32a28975e1362fe2fd4ac4d1cf571c55e8294612
silverbullet233 <3675229+silverbullet233@users.noreply.github.com>
2026-04-18 10:14:21 +0000

    [BugFix] Instantiate ColumnViewer<TYPE_GERMAN_STRING>
    ColumnViewer's explicit instantiation list is driven by
    APPLY_FOR_ALL_SCALAR_TYPE_WITH_NULL, which no longer expands to include
    TYPE_GERMAN_STRING. Add a standalone template-class line alongside HLL /
    OBJECT / PERCENTILE so string-function overloads that use
    ColumnViewer<TYPE_GERMAN_STRING> link.

--- be/src/column/column_viewer.cpp
+++ be/src/column/column_viewer.cpp
@@ -76,5 +76,6 @@ APPLY_FOR_ALL_SCALAR_TYPE_WITH_NULL(M);
 template class ColumnViewer<TYPE_HLL>;
 template class ColumnViewer<TYPE_OBJECT>;
 template class ColumnViewer<TYPE_PERCENTILE>;
+template class ColumnViewer<TYPE_GERMAN_STRING>;
 
 } // namespace starrocks
